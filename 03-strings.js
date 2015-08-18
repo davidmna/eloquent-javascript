94b450629baa18d65001b9a8d6c5de5fc55788f3
David Morales <mrmedinadavid@gmail.com>
2015-08-18 14:44:20 -0500

    Strings

---
+++ 03-strings.js
@@ -0,0 +1,14 @@
+var double_quotes = "Patch my boat with chewing gum";
+    single_quotes = 'Monkeys wave goodbye';
+
+console.log(double_quotes);
+console.log(single_quotes);
+
+var multi_line = "This is the first line\nAnd this is de second";
+console.log(multi_line);
+
+var new_line_char = "A new line character is written like \"\\n\".";
+console.log(new_line_char);
+
+var concatenate = "con" + "cat" + "e" + "nate";
+console.log(concatenate);
